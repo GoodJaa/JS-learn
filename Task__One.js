aed8da8107dc200abce7454bd02f8ed01d284479
Evgeny Golubev <limp061289@gmail.com>
2020-09-10 01:14:26 +0300

    add Task__Three.js file

---
+++ Task__One.js
@@ -0,0 +1,16 @@
+const string = "Привет! Как дела?";
+
+function getVovels(string) {
+    for (i=0; i<=string.length; i++){
+        var letter = string[i];
+        if (letter=="и") {
+            getLetter += letter;
+        } else if (letter=="е") {
+            getLetter += output;
+        } else if (letter=="а"){
+            getLetter += letter;
+        }
+    }
+}
+
+console.log(getLetter);
